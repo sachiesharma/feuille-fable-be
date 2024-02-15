5080635e285486c6544e4672f6c07353c2efd5ef
Sachie Sharma <sachie@gmail.com>
2024-02-15 00:44:46 -0500

    set up server.js with some requests

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 //initialize Express in project
 const express = require("express");
 const app = express();
+
+//when the server receives a GET request to '/'
+app.get("/", (req, res) => {
+  res.send("Express is running!");
+});
+
+//can change this in the future:
+app.get("/greeting", (req, res) => {
+  res.send("Hello World");
+});
+
+//create API endpoint with POST request
+app.post("/endpoint", function (req, res) {
+  //...code that does something
+});
+
+//start Express on port 8080
+app.listen(8080, () => {
+  console.log("Server Started on http://localhost:8080");
+  console.log("Press CTRL + C to stop server");
+});
