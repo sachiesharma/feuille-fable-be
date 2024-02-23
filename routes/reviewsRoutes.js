9b3dbfe3836adb3f9ea32b26a09e977c5d9e1be5
Sachie Sharma <sachie@gmail.com>
2024-02-23 01:19:51 -0500

    adjusted routes to match desired redirection to saved-reviews-page

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const Review = require("../models/Review");
 
 //Route to create a new review
-router.post("/reviews", async (req, res) => {
+router.post("/saved-reviews", async (req, res) => {
   try {
     const { userId, bookId, text, rating } = req.body;
+    //validate?:
+
+    //create review in the database using knex
     const newReview = await Review.create({ userId, bookId, text, rating });
-    res.status(201).json(newReview);
+
+    //redirect to the SavedReviewsPage:
+    res.redirect("/saved-reviews");
+    // res.status(201).json(newReview);
   } catch (error) {
     console.error("Error creating review:", error);
     res.status(500).json({ error: "Internal server error" });
