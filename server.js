cd51ba1c053f08a34fa3bb4725858e2139619df1
Sachie Sharma <sachie@gmail.com>
2024-02-23 22:33:54 -0500

    completed adding the post request

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
+
+let savedReviews = [];
 
 //when the server receives a GET request to '/'
 app.get("/", (req, res) => {
@@ -16,10 +19,27 @@ app.get("/greeting", (req, res) => {
 });
 
 //create API endpoint with POST request
-//change this to review page??
-app.post("/", function (req, res) {
-  res.send("POST request to /");
-  //...code that does something
+
+app.post("/saved-reviews", function (req, res) {
+  const { userId, bookId, text, rating } = req.body;
+  // res.send("POST request to /");
+
+  //Create a new review object
+  const newReview = {
+    userId,
+    bookId,
+    text,
+    rating,
+    createdAt: new Date(),
+  };
+
+  //Add new review to the savedReviews array
+  savedReviews.push(newReview);
+
+  //Send "success" response
+  res
+    .status(201)
+    .json({ message: "Review saved successfully", review: newReview });
 });
 
 //start Express on port 8080
