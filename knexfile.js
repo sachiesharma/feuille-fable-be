a81820af53432484113b5fbbb2e74628b6716330
Sachie Sharma <sachie@gmail.com>
2026-03-11 20:07:01 -0400

    Switch database from MySQL to PostgreSQL/Supabase

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,9 @@
 require("dotenv").config();
 
 module.exports = {
-  client: "mysql2",
+  client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    charset: "utf8",
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
   },
 };
